26afd3764fbf5a8e2b2810e92ef80f4ac2aaddc3
Vince Lynch <autovincelynch@gmail.com>
2019-10-26 12:04:04 +0100

    The google gce comes with both python 2.7 and 3.6
    The pip by default is for python 3 but the salesforce ctrl needs python 2.7
    Need to specify which version of python for pip to use to install cython before installing fastBPE
    sudo python -m pip install Cython

--- install_gce.sh
+++ install_gce.sh
@@ -5,7 +5,7 @@ git clone https://github.com/salesforce/ctrl.git
 mv ctrl/* .
 
 # Cython is needed to compile fastBPE
-sudo pip install Cython
+sudo python -m pip install Cython
 
 # Patch the TensorFlow estimator package
 FILE="/usr/local/lib/python2.7/dist-packages/tensorflow_estimator/python/estimator/keras.py"
